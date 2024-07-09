9917bfb304cca1d9ecc99dc7dee129a072c5870b
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2024-07-09 19:11:35 -0300

    :broom: cleanup: arrumando o codigo
    criando um arquivo para cada componente

---
+++ frontend/src/pages/trip-details/important-links.tsx
@@ -0,0 +1,32 @@
+import { Link2, Plus } from "lucide-react";
+
+export function ImportantLinks() {
+    return (
+        <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links importantes</h2>
+
+            <div className="space-y-5">
+                <div className="flex items-center justify-between gap-4">
+                    <div className="space-y-1.5 flex-1">
+                        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                        <a href="#" className="block text-zinc-400 text-xs truncate hover:text-zinc-200">www.airbnb.com/roasd/asdjbakdjhaksdjhakdjhakjdshaksdjhakdjshakdjhaksjdhaksjdhaksjdh123</a>
+                    </div>
+                    <Link2 className="text-zinc-400 size-5 flex shrink-0" />
+                </div>
+
+                <div className="flex items-center justify-between gap-4">
+                    <div className="space-y-1.5 flex-1">
+                        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                        <a href="#" className="block text-zinc-400 text-xs truncate hover:text-zinc-200">www.airbnb.com/roasd/asdjbakdjhaksdjhakdjhakjdshaksdjhakdjshakdjhaksjdhaksjdhaksjdh123</a>
+                    </div>
+                    <Link2 className="text-zinc-400 size-5 flex shrink-0" />
+                </div>
+            </div>
+
+            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                <Plus className="size-5" />
+                Cadastrar novo item
+            </button>
+        </div>
+    )
+}
