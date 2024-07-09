7f064e3ecd81a8a76cf22c3b27be9105cb3995e3
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2024-07-09 20:08:41 -0300

    :sparkles: feat: componente global
    criar um componente global para os botões, botão com variante (biblioteca tailwind-variant), colocando o componente em todas as rotas

--- frontend/src/pages/create-trip/confirm-trip-modal.tsx
+++ frontend/src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,9 +1,10 @@
 import { User, X } from "lucide-react"
 import { FormEvent } from "react"
+import { Button } from "../../components/button"
 
-interface ConfirmTripModalProps{
+interface ConfirmTripModalProps {
     closeConfirmTripModal: () => void
-    createTrip:  (event: FormEvent<HTMLFormElement>) => void
+    createTrip: (event: FormEvent<HTMLFormElement>) => void
 }
 
 
@@ -38,10 +39,9 @@ export function ConfirmTripModal({
                             name="email"
                             placeholder="Seu e-mail pessoal" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
                     </div>
-
-                    <button type="submit" className=" w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+                    <Button type="submit" variant="primary" size="full">
                         Confirmar criação da viagem
-                    </button>
+                    </Button>
                 </form>
             </div>
         </div>
